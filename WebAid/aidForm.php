3b7dc4940303cc17854b8e882c61652dc643c624
TerrorTurtle <te14.carl.leong@umea.nti.se>
2017-04-11 13:00:54 +0200

    1

---
+++ WebAid/aidForm.php
@@ -0,0 +1,25 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "webAidDb";
+
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+$sql = "INSERT INTO form (orgname, logo, info, focus, designWish, deadWish, mail, tele)
+VALUES ('$_POST[orgname]', '$_POST[logo]', '$_POST[info]', '$_POST[focus]', '$_POST[designWish]', '$_POST[deadWish]', '$_POST[mail]', '$_POST[tele]')";
+
+if (mysqli_query($conn, $sql)) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+
+mysqli_close($conn);
+?>
